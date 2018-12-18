0dbc0f903e452dc4babc984f4b7a572d19261d58
alemolin <alemolin@DESB1110.tse.ads.t-systems.es>
2018-12-18 16:49:49 +0100

    Creating maven project with modules.

---
+++ parent-DemoMaven/Demo-jar/src/main/java/com/DemoMaven/multimodule/App.java
@@ -0,0 +1,13 @@
+package com.DemoMaven.multimodule;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ parent-DemoMaven/README.md
@@ -0,0 +1,2 @@
+# DemoMavenDAOandWebService
+At this project I will work with Maven and some Anotation APIs to create a simple web application, with Web Services too.
